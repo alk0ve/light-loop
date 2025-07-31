a21318cfbf3bdd1786993d3c66db04707eed7e7e
Alexei Kovelman <alexei.kovelman@gmail.com>
2025-07-31 21:52:18 +0300

    anchor sprites on the center of the image

--- assets/asset_loader.py
+++ assets/asset_loader.py
@@ -2,4 +2,7 @@ import pyglet
 
 
 def load_image(name: str):
-    return pyglet.resource.image(name)
+    image = pyglet.resource.image(name)
+    image.anchor_x = image.width // 2
+    image.anchor_y = image.height // 2
+    return image
